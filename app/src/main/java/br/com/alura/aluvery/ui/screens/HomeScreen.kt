71569f3da0e968b1f187a044d98ff135d5b0072b
gutodidonato <gutodidonato@gmail.com>
2025-02-24 12:43:57 -0300

    feat: treino de state hoisting pelo alura

---
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -0,0 +1,48 @@
+package br.com.alura.aluvery.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.alura.aluvery.model.Product
+import br.com.alura.aluvery.sampledata.sampleSections
+import br.com.alura.aluvery.ui.components.ProductsSection
+import br.com.alura.aluvery.ui.theme.AluveryTheme
+
+@Composable
+fun HomeScreen(
+        sections: Map<String, List<Product>>
+) {
+    LazyColumn(
+            Modifier
+                    .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+    ) {
+        for (section in sections) {
+            val title = section.key
+            val products = section.value
+            item {
+                ProductsSection(
+                        title = title,
+                        products = products
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    AluveryTheme {
+        Surface {
+            HomeScreen(sampleSections)
+        }
+    }
+}
